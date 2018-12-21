81d0948dc75d4f0aba9cdc2aa2b63ccb67062622
Andy Alleyne <andyalleyne@andys-macbook-pro.local>
2018-12-21 13:11:41 -0500

    started the final version

---
+++ MarketPro/Controller/DetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailTableViewCell.swift
+//  MarketPro
+//
+//  Created by Andy Alleyne on 12/21/18.
+//  Copyright © 2018 AlleyneVentures. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MarketPro/Model/Stock.swift
@@ -0,0 +1,47 @@
+//
+//  Stock.swift
+//  MarketPro
+//
+//  Created by Andy Alleyne on 12/21/18.
+//  Copyright © 2018 AlleyneVentures. All rights reserved.
+//
+
+import Foundation
+
+class Stock {
+    
+    var symbol = "-"
+    var companyName = "-"
+    var primaryExchange = "-"
+    var sector = "-"
+    var calculationPrice = "-"
+    var open = "-"
+    var openTime = "-"
+    var close = "-"
+    var closeTime = "-"
+    var high = "-"
+    var low = "-"
+    var latestPrice = "-"
+    var latestSource = "-"
+    var latestTime = "-"
+    var latestUpdate = "-"
+    var latestVolume = "-"
+    var iexRealTimePrice = "-"
+    var iexRealTimeSize = "-"
+    var iexLatestUpdate = "-"
+    var delayedPrice = "-"
+    var delayedPriceTime = "-"
+    var change = "-"
+    var changePercent = "-"
+    var iexVolume = "-"
+    var avgTotalVolume = "-"
+    var peRation = "-"
+    var week52High = "-"
+    var week52Low = "-"
+    var ytdChange = "-"
+    var bidPrice = "-"
+    var bidSize = "-"
+    var askPrice = "-"
+    var askSize = "-"
+    
+}
