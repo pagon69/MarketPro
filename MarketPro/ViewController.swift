08101b3cecbc19414ad62aae7e702b70cdf73d66
Andy Alleyne <andyalleyne@andys-macbook-pro.local>
2018-12-21 12:35:43 -0500

    Initial Commit

---
+++ MarketPro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarketPro
+//
+//  Created by Andy Alleyne on 12/21/18.
+//  Copyright © 2018 AlleyneVentures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
